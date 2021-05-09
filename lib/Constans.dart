e57b99abe6c03b787e101db1527ab5cac1682c09
Enesksgl <ksglns@gmail.com>
2021-05-09 23:39:18 +0300

    some improvement

---
+++ lib/Constans.dart
@@ -0,0 +1,11 @@
+//import 'models/UserModel.dart';
+
+class Constants {
+  static String BASE_URL = "http://192.168.0.109:8080";
+  static String IMAGE_BASE_URL = "/api/files/downloadFile/";
+  static String BEARER_TOKEN =
+      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyOTIzMTEwMywiaWF0IjoxNjIwNTkxMTAzfQ.7n_Rc6LLMiERw3ybcmk8bHDbQ___f6GWCTxscciSllo";
+  static int USER_ID = 0;
+  //static UserModel USER;
+  static int IMAGE_ID = 0;
+}
