f7c2f456e1125a60b955b506b5278a0034076563
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-05-17 21:33:00 +0300

    file renamed

--- lib/GlobalVariables.dart
+++ lib/GlobalVariables.dart
@@ -3,7 +3,7 @@ import 'package:yemek_tarifi_odev_mobil/models/IngredientModel.dart';
 
 import 'models/UserModel.dart';
 
-class Constants {
+class GlobalVariables {
   static String SEARCHED="";
   static String BASE_URL = "http://192.168.0.21:8080";
   static String IMAGE_BASE_URL = "/api/files/downloadFile/";
